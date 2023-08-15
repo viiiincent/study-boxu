1c7ab3bb7bb7d69b460724603d49df07d5ed1b53
Vincent Marger <vincent.marger@gmail.com>
2023-08-15 14:15:43 -0400

    Add automatic form parsing package

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -44,4 +47,22 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
 	}
+}
+
+func (app *application) decodePostForm(r *http.Request, dest any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dest, r.PostForm)
+	if err != nil {
+		var invalidDecoderError *form.InvalidDecoderError
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+		return err
+	}
+	
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example/snippetbox
 go 1.20
 
 require (
+	github.com/go-playground/form/v4 v4.2.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 )
