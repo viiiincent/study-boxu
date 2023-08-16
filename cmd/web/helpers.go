93697fa7d3b15777dc1d366dc55c5679be5c6565
Vincent Marger <vincent.marger@gmail.com>
2023-08-15 21:15:52 -0400

    Add HTTPS support

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -45,7 +45,8 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
+		CurrentYear:	time.Now().Year(),
+		Flash:			app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -49,6 +49,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 	app := &application{
 		errorLog:		errorLog,
 		infoLog:		infoLog,
@@ -58,14 +59,23 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr: *addr,
-		ErrorLog: errorLog,
-		Handler: app.routes(),
+		Addr: 		*addr,
+		ErrorLog: 	errorLog,
+		Handler: 	app.routes(),
+		TLSConfig:	tlsConfig,
+		IdleTimeout:	time.Minute,
+		ReadTimeout: 	5 * time.Second,
+		WriteTimeout: 	10 * time.Second,
+
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
